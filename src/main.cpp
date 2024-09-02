0b958c5df7db36e0c9fe48f418bef6ec601b6438
Tamo <buzzoopa@gmail.com>
2024-09-02 10:37:25 +0100

    SDL baseline

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+static bool isRunning = true;
+bool createWiindow(int width, int height, const char* title);
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
